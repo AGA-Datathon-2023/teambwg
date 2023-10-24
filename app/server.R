45e035902a610b6a1e7e38bdf3e6aa6c6cbb8427
Greg LeVay <68603996+gdlevay@users.noreply.github.com>
2023-10-24 11:36:06 -0400

    Removed unnecessary function

--- app/server.R
+++ app/server.R
@@ -1,5 +1,4 @@
 server <- function(input, output, session) {
-  source("C:/Users/gdlev/Desktop/Ideas/aga/shiny.R")
   output$dropdown <- renderText(input$mtcars_dropdown)
   output$agency_perc <- renderEcharts4r({
     graph_agency(agency_base, input$agencyselect)$e
